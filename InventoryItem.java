16b4a7a88d9903e15888270325c343c71bedf1ab
kudos07 <58479988+kudos07@users.noreply.github.com>
2020-10-29 23:36:10 +0530

    Add files via upload

---
+++ InventoryItem.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package home.inventory.project;
+
+/**
+ *
+ * @author 91912
+ */
+class InventoryItem {
+    public String description;
+public String location;
+public boolean marked;
+//Suven Internship
+public String serialNumber;
+public String purchasePrice;
+public String purchaseDate;
+public String purchaseLocation;
+public String note;
+public String photoFile;
+    
+}
---
+++ dateDateChooser.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package home.inventory.project;
+
+/**
+ *
+ * @author 91912
+ */
+class dateDateChooser {
+    
+}
